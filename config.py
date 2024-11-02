1b3e757c19872a51b635c99f9d5e6a6bcb9d60e7
Shantel <williamsshantel35@yahoo.com>
2024-11-01 20:13:25 -0700

    removed private items

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 browser = ["Chrome", "Microsoft Edge"]
 # Enter your Linkedin password and username below. Do not commit this file after entering these credentials.
 # Linkedin credentials
-email = "williamsshantel35@yahoo.com"
-password = "U!GbVhc!nmgT69s"
+email = ""
+password = ""
 
 #PRO FEATURE - Optional! run browser in headless mode, no browser screen will be shown it will work in background.
 headless = False
@@ -18,15 +18,15 @@ chromeProfilePath = r""
 # These settings are for running Linkedin job apply bot.
 # location you want to search the jobs - ex : ["Poland", "Singapore", "New York City Metropolitan Area", "Monroe County"]
 # continent locations:["Europe", "Asia", "Australia", "NorthAmerica", "SouthAmerica", "Africa", "Australia"]
-location = ["NorthAmerica"]
+location = ["San Diego", "Arizona", "San Fransisco", "Simi Valley", "Seattle, Washington", "Texas", "Atlanta", "Los Angeles", "Orange County", "Burbank" ]
 # keywords related with your job search
 keywords = ["frontend", "react", "backend","javascript", "mongoDB", "python", "programming", "API", "Software Development"]
 #job experience Level - ex:  ["Internship", "Entry level" , "Associate" , "Mid-Senior level" , "Director" , "Executive"]
-experienceLevels = [ "Entry level" ]
+experienceLevels = [ "Entry level","Internship", "Apprenticeship" ]
 #job posted date - ex: ["Any Time", "Past Month" , "Past Week" , "Past 24 hours"] - select only one
 datePosted = ["Past Week"]
 #job type - ex:  ["Full-time", "Part-time" , "Contract" , "Temporary", "Volunteer", "Intership", "Other"]
-jobType = ["Full-time", "Part-time" , "Contract"]
+jobType = ["Full-time", "Part-time" , "Contract","Internship", "Apprenticeship"]
 #remote  - ex: ["On-site" , "Remote" , "Hybrid"]
 remote = ["On-site" , "Remote" , "Hybrid"]
 #salary - ex:["$40,000+", "$60,000+", "$80,000+", "$100,000+", "$120,000+", "$140,000+", "$160,000+", "$180,000+", "$200,000+" ] - select only one
